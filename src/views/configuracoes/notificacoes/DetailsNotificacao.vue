65c4c6e2e7140b09fa1aff581c857ed059bb5216
neiltonrodriguez <neiltonrodriguez@gmail.com>
2024-04-23 13:19:00 -0300

    corrigindo erro no update de notificações

--- src/views/configuracoes/notificacoes/DetailsNotificacao.vue
+++ src/views/configuracoes/notificacoes/DetailsNotificacao.vue
@@ -31,46 +31,46 @@
                 </div>
                 <div>
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Destinatário</label>
-                    <input type="text" v-model="notificacao.destinatario" disabled v-on:keyup="habilitarSalvar()"
+                    <input type="text" v-model="notificacao.destinatario" v-on:keyup="habilitarSalvar()"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  disabled:bg-red-50 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="" required>
                 </div>
                 <div>
 
-<label class="relative inline-flex items-center cursor-pointer">
-    <input @click="habilitarSalvar()" v-model="notificacao.notificacc" type="checkbox" :value="false"
-        class="sr-only peer">
-    <div
-        class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
-    </div>
-    <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Notificar CC</span>
-</label>
-</div>
-<div>
-<label class="relative inline-flex items-center cursor-pointer">
-    <input @click="habilitarSalvar()" disabled v-model="notificacao.nottificacco" type="checkbox"
-        :value="false" class="sr-only peer">
-    <div
-        class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
-    </div>
-    <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Notificar CCO</span>
-</label>
-</div>
+                    <label class="relative inline-flex items-center cursor-pointer">
+                        <input @click="habilitarSalvar()" v-model="notificacao.notificacc" type="checkbox"
+                            :value="false" class="sr-only peer">
+                        <div
+                            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                        </div>
+                        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Notificar CC</span>
+                    </label>
+                </div>
+                <div>
+                    <label class="relative inline-flex items-center cursor-pointer">
+                        <input @click="habilitarSalvar()" disabled v-model="notificacao.nottificacco" type="checkbox"
+                            :value="false" class="sr-only peer">
+                        <div
+                            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                        </div>
+                        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Notificar CCO</span>
+                    </label>
+                </div>
                 <div>
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Com Cópia</label>
                     <input type="text" v-model="notificacao.destinatariocc" v-on:keyup="habilitarSalvar()"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  disabled:bg-red-50 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="" required>
+                        placeholder="">
                 </div>
                 <div>
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Com Cópia Oculta</label>
                     <input type="text" v-model="notificacao.destinatariocco" v-on:keyup="habilitarSalvar()"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  disabled:bg-red-50 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="" required>
+                        placeholder="">
                 </div>
                 <div>
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mensagem</label>
-                    <textarea rows="4" v-model="notificacao.mensagem"
+                    <textarea rows="4" v-model="notificacao.mensagem" v-on:keyup="habilitarSalvar()"
                         class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="digite aqui sua mensagem"></textarea>
                 </div>
@@ -108,7 +108,8 @@
                     </select>
                 </div>
                 <div>
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mensagem Integração</label>
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mensagem
+                        Integração</label>
                     <select v-model="notificacao.mensagemintegracao" @change="habilitarSalvar()"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="" disabled selected>Escolha um</option>
@@ -116,7 +117,7 @@
                     </select>
                 </div>
 
-                
+
 
             </div>
             <button type="submit" v-bind:disabled="isDisabled"
@@ -155,6 +156,9 @@ export default {
                 is_active: ts.is_active,
                 mensagemintegracao: ts.mensagemintegracao,
                 emailintegracao: ts.emailintegracao,
+                destinatario: ts.destinatario,
+                destinatariocc: ts.destinatariocc,
+                destinatariocco: ts.destinatariocco,
                 tipoenvio: ts.tipoenvio,
                 notificacc: ts.notificacc,
             }
@@ -228,7 +232,7 @@ export default {
             http.get('listaitens/conta/lista?so_ativos=true')
                 .then(res => {
                     this.contas = res.data.results
-                    
+
                 })
                 .catch(e => {
                     this.$swal("Oops...", e.response.data.detail, "error");
