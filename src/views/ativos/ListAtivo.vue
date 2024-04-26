716f95983a5ba0667c5b1b4f8c6a8398552f36a2
neiltonrodriguez <neiltonrodriguez@gmail.com>
2024-04-26 08:49:31 -0300

    colocando botão selecinoar todos

--- src/views/ativos/ListAtivo.vue
+++ src/views/ativos/ListAtivo.vue
@@ -8,104 +8,123 @@
             <button @click="accessRoute('create-ativo')"
                 class="px-4 py-2 font-semibold text-sm bg-blue-800 text-white rounded-md shadow-sm">Novo
                 Ativo</button>
+            <button :disabled="idsForDelete.length === 0" @click="deleteSelecionados()"
+                class="px-4 mx-3 py-2 font-semibold text-sm bg-red-800 disabled:bg-gray-300 text-white rounded-md shadow-sm">Deletar
+                Selecionados</button>
         </div>
+
         <div class="h-[500px] overflow-y-auto">
-        <table class="border-collapse table-fixed w-full text-sm">
-            <thead>
-                <tr>
-                    <th
-                        class="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        ICONE</th>
-                    <th
-                        class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        Nome</th>
-                    <th
-                        class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        Serial</th>
-                    <th v-if="user.tipo == 4"
-                        class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        Conta</th>
-                    <th v-if="user.tipo == 4"
-                        class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        Criado por</th>
-                    <th
-                        class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        Habilitado</th>
-                    <th
-                        class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        Ação</th>
 
-                </tr>
-            </thead>
-            <tbody class="bg-white dark:bg-slate-800">
-                <tr class="hover:bg-gray-100 cursor-pointer duration-200" v-for="a in ativos" :key="a.id">
-                    <td @click="detailsAtivo(a.id)"
-                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
-                        <template v-if="a.icone == null"><img class="rounded-lg shadow-md duration-200 hover:scale-105"
-                                width="40" src="../../assets/img/sem-foto.png"> </template>
-                        <template v-else>
-                            <img class="rounded-lg shadow-md duration-200 hover:scale-105" :src="a.icone" width="40">
-                        </template>
+            <table class="border-collapse table-fixed w-full text-sm">
+                <thead>
+                    <tr>
+                        <th
+                            class="border-b dark:border-slate-600 font-medium p-2 text-slate-400 dark:text-slate-200 text-left">
+                            <input type="checkbox" id="checkAll" @click="checkAllItems()" :value="true">
+                        </th>
+                        <th
+                            class="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            ICONE</th>
+                        <th
+                            class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            Nome</th>
+                        <th
+                            class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            Serial</th>
+                        <th v-if="user.tipo == 4"
+                            class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            Conta</th>
+                        <th v-if="user.tipo == 4"
+                            class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            Criado por</th>
+                        <th
+                            class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            Habilitado</th>
+                        <th
+                            class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            Ação</th>
 
-                    </td>
-                    <td @click="detailsAtivo(a.id)"
-                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
-                        {{ a.nome }}</td>
-                    <td @click="detailsAtivo(a.id)"
-                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
-                        {{ a.serial }}</td>
-                    <td @click="detailsAtivo(a.id)" v-if="user.tipo == 4"
-                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
-                        {{ a.conta }}</td>
-                    <td @click="detailsAtivo(a.id)" v-if="user.tipo == 4"
-                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
-                        {{ a.criadopor }}</td>
-                    <td @click="detailsAtivo(a.id)"
-                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
-                        <template v-if="a.is_active">
-                            <div class="flex items-center">
-                                <div class="h-2.5 w-2.5 rounded-full                     bg-green-500 mr-2"></div> Sim
-                            </div>
-                        </template>
-                        <template v-else>
-                            <div class="flex items-center">
-                                <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> Não
-                            </div>
-                        </template>
-                    </td>
-                    <td
-                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
-                        <button @click="detailsAtivo(a.id)" class="btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                            </svg>
-                        </button>
-                        <button @click="deleteAtivo(a.id)" class="btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                            </svg>
+                    </tr>
+                </thead>
+                <tbody class="bg-white dark:bg-slate-800">
+                    <tr class="hover:bg-gray-100 cursor-pointer duration-200" v-for="a in ativos" :key="a.id">
+                        <td 
+                            class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                            <input type="checkbox" :id="'check' + a.id" :value="a.id" v-model="idsForDelete">
+                        </td>
+                        <td @click="detailsAtivo(a.id)"
+                            class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                            <template v-if="a.icone == null"><img
+                                    class="rounded-lg shadow-md duration-200 hover:scale-105" width="40"
+                                    src="../../assets/img/sem-foto.png"> </template>
+                            <template v-else>
+                                <img class="rounded-lg shadow-md duration-200 hover:scale-105" :src="a.icone"
+                                    width="40">
+                            </template>
+
+                        </td>
+                        <td @click="detailsAtivo(a.id)"
+                            class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                            {{ a.nome }}</td>
+                        <td @click="detailsAtivo(a.id)"
+                            class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                            {{ a.serial }}</td>
+                        <td @click="detailsAtivo(a.id)" v-if="user.tipo == 4"
+                            class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                            {{ a.conta }}</td>
+                        <td @click="detailsAtivo(a.id)" v-if="user.tipo == 4"
+                            class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                            {{ a.criadopor }}</td>
+                        <td @click="detailsAtivo(a.id)"
+                            class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                            <template v-if="a.is_active">
+                                <div class="flex items-center">
+                                    <div class="h-2.5 w-2.5 rounded-full                     bg-green-500 mr-2"></div>
+                                    Sim
+                                </div>
+                            </template>
+                            <template v-else>
+                                <div class="flex items-center">
+                                    <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> Não
+                                </div>
+                            </template>
+                        </td>
+                        <td
+                            class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                            <button @click="detailsAtivo(a.id)" class="btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
+                                    stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                            </button>
+                            <button @click="deleteAtivo(a.id)" class="btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
+                                    stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                </svg>
 
 
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-        <div class="py-4">
-            <button v-for="(page, index) in pages" :key="page"
-                class="px-3 mx-1 py-2 text-sm bg-blue-700 hover:bg-blue-900 focus:bg-violet-700 text-white"
-                :class="{ current: page === current }" @click="changePage(index)">
-                {{ page }}
-            </button>
-            <span class="mx-4 text-xd font-semibold text-blue-700">total de registros:</span> {{ total }}
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="py-4">
+                <button v-for="(page, index) in pages" :key="page"
+                    class="px-3 mx-1 py-2 text-sm bg-blue-700 hover:bg-blue-900 focus:bg-violet-700 text-white"
+                    :class="{ current: page === current }" @click="changePage(index)">
+                    {{ page }}
+                </button>
+                <span class="mx-4 text-xd font-semibold text-blue-700">total de registros:</span> {{ total }}
+            </div>
+            
         </div>
+        
+
     </div>
 </template>
 
@@ -130,10 +149,12 @@ export default {
             ativos: [],
             contas: [],
             empresas: [],
+            idsForDelete: [],
             offset: 0,
             limit: 10,
             pages: [],
             total: 0,
+            items: {},
             filter: {
                 search: '',
                 departamento: 0,
@@ -206,6 +227,60 @@ export default {
         changePage(i) {
             this.getAll(i);
             this.offset = 0
+        },
+        checkAllItems(){
+            const element = document.getElementById('checkAll')
+            let arr = []
+            if (element.checked){
+                for(let x = 0; x < this.ativos.length; x++){
+                    console.log(x);
+                   const e = document.getElementById('check' + this.ativos[x].id);
+                   e.checked = true
+                   arr.push(e.value)
+                }
+                // this.idsForDelete = arr
+            } else {
+                for(let i = 0; i < this.ativos.length; i++){
+                   const e = document.getElementById('check' + this.ativos[i].id);
+                   e.checked = false
+                }
+                arr = []
+            }
+
+            this.idsForDelete = arr
+        },
+        deleteSelecionados() {
+            this.items.items = this.idsForDelete
+            this.$swal.fire({
+                title: 'Deseja Realmente Excluir todos os selecionados?',
+                text: "Essa ação não pode ser revertida!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: 'Cancelar',
+                confirmButtonText: 'Sim, Excluir!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    http.delete('/ativos/itens/delete/', this.items)
+                        .then(res => {
+                            this.getAll();
+                            this.$swal.fire({
+                                icon: 'success',
+                                title: res.data.detail,
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                        })
+                        .catch(e => {
+                            this.$swal("Oops...", e.response.data.detail, "error");
+                            if (e.response.data.detail == "Você não tem permissão para executar essa ação.") {
+                                this.$router.push('/')
+                            }
+                        });
+                }
+            })
+
         },
         filtrar(payload) {
             this.filter = payload;
--- src/views/tipoativos/ListTipoAtivo.vue
+++ src/views/tipoativos/ListTipoAtivo.vue
@@ -8,17 +8,24 @@
             <button @click="accessRoute('create-tipoativo')"
                 class="px-4 py-2 font-semibold text-sm bg-blue-800 text-white rounded-md shadow-sm">Novo
                 tipo de ativo</button>
+                <button :disabled="idsForDelete.length === 0" @click="deleteSelecionados()"
+                class="px-4 mx-3 py-2 font-semibold text-sm bg-red-800 disabled:bg-gray-300 text-white rounded-md shadow-sm">Deletar
+                Selecionados</button>
         </div>
         <table class="border-collapse table-fixed w-full text-sm">
             <thead>
                 <tr>
+                    <th
+                        class="border-b dark:border-slate-600 font-medium p-2 text-slate-400 dark:text-slate-200 text-left">
+                        <input type="checkbox" id="checkAll" @click="checkAllItems()" :value="true">
+                    </th>
                     <th
                         class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
                         Ícone</th>
                     <th
                         class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
                         Nome</th>
-                    
+
                     <th v-if="user.tipo == 4"
                         class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
                         Conta</th>
@@ -35,9 +42,12 @@
                 </tr>
             </thead>
             <tbody class="bg-white dark:bg-slate-800">
-                <tr class="hover:bg-gray-100 cursor-pointer duration-200" 
-                    v-for="a in tipoativos" :key="a.id">
-                    <td @click="detailsTipoAtivo(a.id)" class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                <tr class="hover:bg-gray-100 cursor-pointer duration-200" v-for="a in tipoativos" :key="a.id">
+                    <td class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                        <input type="checkbox" :id="'check' + a.id" :value="a.id" v-model="idsForDelete">
+                    </td>
+                    <td @click="detailsTipoAtivo(a.id)"
+                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
                         <template v-if="a.icone == null"><img class="rounded-lg shadow-md duration-200 hover:scale-105"
                                 width="40" src="../../assets/img/sem-foto.png"> </template>
                         <template v-else>
@@ -47,7 +57,7 @@
                     <td @click="detailsTipoAtivo(a.id)"
                         class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
                         {{ a.nome }}</td>
-                    
+
                     <td @click="detailsTipoAtivo(a.id)" v-if="user.tipo == 4"
                         class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
                         {{ a.conta }}</td>
@@ -67,8 +77,7 @@
                             </div>
                         </template>
                     </td>
-                    <td
-                        class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
+                    <td class="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-slate-400">
                         <button @click="detailsTipoAtivo(a.id)" class="btn">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                                 stroke="currentColor" class="w-6 h-6">
@@ -130,6 +139,8 @@ export default {
             limit: 10,
             pages: [],
             total: 0,
+            idsForDelete: [],
+            items: {},
             filter: {
                 search: '',
                 departamento: 0,
@@ -143,6 +154,60 @@ export default {
         };
     },
     methods: {
+        checkAllItems(){
+            const element = document.getElementById('checkAll')
+            let arr = []
+            if (element.checked){
+                for(let x = 0; x < this.tipoativos.length; x++){
+                    console.log(x);
+                   const e = document.getElementById('check' + this.tipoativos[x].id);
+                   e.checked = true
+                   arr.push(e.value)
+                }
+                // this.idsForDelete = arr
+            } else {
+                for(let i = 0; i < this.tipoativos.length; i++){
+                   const e = document.getElementById('check' + this.tipoativos[i].id);
+                   e.checked = false
+                }
+                arr = []
+            }
+
+            this.idsForDelete = arr
+        },
+        deleteSelecionados() {
+            this.items.items = this.idsForDelete
+            this.$swal.fire({
+                title: 'Deseja Realmente Excluir todos os selecionados?',
+                text: "Essa ação não pode ser revertida!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: 'Cancelar',
+                confirmButtonText: 'Sim, Excluir!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    http.delete('/ativotipos/itens/delete/', this.items)
+                        .then(res => {
+                            this.getAll();
+                            this.$swal.fire({
+                                icon: 'success',
+                                title: res.data.detail,
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                        })
+                        .catch(e => {
+                            this.$swal("Oops...", e.response.data.detail, "error");
+                            if (e.response.data.detail == "Você não tem permissão para executar essa ação.") {
+                                this.$router.push('/')
+                            }
+                        });
+                }
+            })
+
+        },
         getAll(i = null) {
             if (i) {
                 this.offset = i
